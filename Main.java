222eae2bc65839fb0491c03f30da6d929501ecbf
Patryk Balcerowski <patrykbalc@gmail.com>
2021-01-06 16:57:29 +0100

    Pierwszy commit

---
+++ Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main{
+
+    public static void main(String[] args){
+        System.out.println("Podaj imie:");
+        Scanner nameScanner = new Scanner(System.in);
+        String userName = nameScanner.nextLine();
+        System.out.println("Twoje imię to: "+userName);
+        String userLastname = nameScanner.nextLine();
+        System.out.println("Imie: "+userName+"\nNazwisko: "+userLastname);
+
+        List<String> userAges = new ArrayList<>();
+    }
+
+}
